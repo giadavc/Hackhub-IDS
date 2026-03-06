68a4412b890cc62eac5d8222236f5fd3d9bdc62e
Giada Vecchi <giada.vecchi@studenti.unicam.it>
2026-03-06 16:24:46 +0100

    inizio

---
+++ src/main/java/org/example/hackhubids/HackhubIdsApplication.java
@@ -0,0 +1,13 @@
+package org.example.hackhubids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackhubIdsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackhubIdsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/hackhubids/HackhubIdsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hackhubids;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackhubIdsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
