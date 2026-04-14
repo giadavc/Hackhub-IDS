191993f11e7fac8ade0912d7b54f553b0a62cfb3
Nicola <nicola.lobascio@studenti.unicam.it>
2026-04-14 18:20:55 +0200

    Fix iscrizione ad un solo hackathon per team

--- src/main/java/org/example/hackhubids/Repository/HackathonRegistrationRepository.java
+++ src/main/java/org/example/hackhubids/Repository/HackathonRegistrationRepository.java
@@ -1,13 +1,16 @@
 package org.example.hackhubids.Repository;
 
+import java.util.Collection;
+
 import org.example.hackhubids.Domain.Hackathon;
 import org.example.hackhubids.Domain.HackathonRegistration;
 import org.example.hackhubids.Domain.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.example.hackhubids.Domain.HackathonStatus;
 
 public interface HackathonRegistrationRepository extends JpaRepository<HackathonRegistration, Long> {
 
-    boolean existsByHackathonAndTeam(Hackathon hackathon, Team team);
+    boolean existsByTeamAndHackathon_StatusIn(Team team, Collection<HackathonStatus> statuses);
 
     long countByHackathon(Hackathon hackathon);
 }
