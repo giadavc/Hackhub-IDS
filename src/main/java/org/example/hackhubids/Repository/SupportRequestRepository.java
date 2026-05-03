516fd221616af994e153a7b797975c6f5dfb948d
Luca Prosperi <luca.prosperi@studenti.unicam.it>
2026-05-03 15:49:33 +0200

    SupportRequestRepository

--- src/main/java/org/example/hackhubids/Repository/SupportRequestRepository.java
+++ src/main/java/org/example/hackhubids/Repository/SupportRequestRepository.java
@@ -2,7 +2,6 @@ package org.example.hackhubids.Repository;
 
 import java.util.List;
 
-import org.example.hackhubids.Domain.StaffMember;
 import org.example.hackhubids.Domain.SupportRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
